52d39e56c127d66cdb529a0ce107eaa0f1c2c501
Levani Mshvildadze <levani.mshvildadze16@gmail.com>
2025-01-28 15:47:38 +0400

    Add files via upload

--- E/script.js
+++ E/script.js
@@ -5,3 +5,16 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 });
+
+
+const header = document.querySelector('.header'); // Select your header element
+const stickyClass = 'sticky'; // Define the class that will make it sticky
+
+// Function to add the sticky class when scrolling
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add(stickyClass);
+  } else {
+    header.classList.remove(stickyClass);
+  }
+});
